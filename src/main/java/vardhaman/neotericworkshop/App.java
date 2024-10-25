64baefaf36bc9695c2561b7e3138fbe57b04165f
Abhi-116 <@Abhi-116>
2024-10-25 15:59:53 +0530

    demo

---
+++ src/main/java/vardhaman/neotericworkshop/App.java
@@ -0,0 +1,13 @@
+package vardhaman.neotericworkshop;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
